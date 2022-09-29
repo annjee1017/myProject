fb82e45e240c68bb9ddafdc5959c27435860d9eb
Hyunji Kim <58774260+annjee1017@users.noreply.github.com>
2022-09-29 18:51:14 +0900

    compile all .py files and add backup function

--- compile.py
+++ compile.py
@@ -1,10 +1,31 @@
-# python compile.py -i main_master
+import os
+import re
+import py_compile as pc
+from datetime import datetime
+import shutil
 
-import py_compile
-import argparse
+a = list(os.walk(os.getcwd()))
+backup_folder = str(datetime.strftime(datetime.now(), '%y%m%d_%H%M%S'))
+final_backup_dir = os.path.join('.backup', backup_folder)
+os.makedirs(final_backup_dir, exist_ok=True)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input_filename", required=True,	help="filename for compile")
-args = vars(ap.parse_args())
+for dumps in a:
+    total_dir, folders, files = dumps
+    for j in [os.path.join(total_dir, i) for i in files]:
+        # continue if file is compiler
+        if 'compiler.py' in j:
+            continue
+        if '.py' not in j:
+            continue
+        print('[Current file]', j)
 
-py_compile.compile(f"{(args['input_filename'])}.py", cfile=f"{(args['input_filename'])}.pyc")
+        # compile every <.py> to <.pyc>
+        kk = pc.compile(j, cfile=re.sub('.py$', '.pyc', j))
+        backup_j = j.split(os.sep)[-1]
+        print(backup_j)
+
+        # backup files
+        backup_folder = str(datetime.strftime(datetime.now(), '%y%m%d_%H%M%S'))
+        final_backup_dir = os.path.join('.backup', backup_folder)
+        os.makedirs(final_backup_dir, exist_ok=True)
+        shutil.move(j, os.path.join(final_backup_dir, backup_j))
